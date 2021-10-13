534cfdf6902194115356f54efa7733265bc88e35
LOctavio <luis_ram1997@hotmail.com>
2021-10-13 12:06:28 -0500

    Add relationship between Person and Rental

--- rental.rb
+++ rental.rb
@@ -4,10 +4,15 @@ class Rental
   def initialize(date)
     @date = date
     @book = "Uknown"
+    @person = "Uknown"
   end
 
   def book=(book)
     @book = book
     book.rental.push(self) unless book.rental.include?(self)
   end
+
+  def person=(person)
+    @person = person
+    person.rental.push(self) unless person.rental.include?(self)
 end
