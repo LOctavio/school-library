fdbbd1a21fc8869a54f945daf07e5dcb8c79b3db
LOctavio <luis_ram1997@hotmail.com>
2021-10-14 13:48:52 -0500

    Add method to list all people

--- main.rb
+++ main.rb
@@ -7,4 +7,9 @@ class Main
   def show_books
     @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
   end
+
+  def show_people
+    @persons.each { |person| puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+  end
+
 end
