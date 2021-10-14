3de46298bd47083e7bcee45b2a39d8261906451e
LOctavio <luis_ram1997@hotmail.com>
2021-10-14 15:23:30 -0500

    Add method create a rental

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative './student'
 require_relative './teacher'
 require_relative './book'
+require_relative './rental'
 
 class Main
   def initialize
@@ -41,5 +42,22 @@ class Main
     author = gets.chomp
     @books.push(Book.new(title, author))
     puts 'Book created successfully'
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}" }
+    book = gets.chomp.to_i
+    puts 'Select a person from the following list by number'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person = gets.chomp.to_i
+    puts 'Date:'
+    date = gets.chomp
+    rental = Rental.new(date)
+    rental.book = @books[book]
+    @people[person].add_rental(rental)
+    puts 'Rental created successfully'
   end
 end
