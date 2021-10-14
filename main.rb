2849e7aabc7ad8c2ec3658c2acf0caef8e66f565
LOctavio <luis_ram1997@hotmail.com>
2021-10-14 15:19:52 -0500

    Change name of instance variable

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require_relative './book'
 class Main
   def initialize
     @books = []
-    @persons = []
+    @people = []
   end
 
   def show_books
@@ -13,23 +13,23 @@ class Main
   end
 
   def show_people
-    @persons.each { |person| puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    @people.each { |person| puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   end
 
   def create_person(person)
     puts 'Age:'
-    age = gets.chomp
+    age = gets.chomp.to_i
     puts 'Name:'
     name = gets.chomp
     case person
     when 1
       puts 'Has parent permission? [Y/N]:'
       parent_permission = gets.chomp
-      @persons.push(Student.new(age, name, parent_permission))
+      @people.push(Student.new(age, name, parent_permission))
     when 2
       puts 'Specialization:'
       specialization = gets.chomp
-      @persons.push(Teacher.new(age, name, specialization))
+      @people.push(Teacher.new(age, name, specialization))
     end
     puts 'Person created successfully'
   end
