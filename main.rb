bc625e8c1d3ad8d9a1d5e5682a7cd95b389fe854
LOctavio <luis_ram1997@hotmail.com>
2021-10-14 13:51:57 -0500

    Add method create a person

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+require_relative './student'
+require_relative './teacher'
+
 class Main
   def initialize
     @books = []
@@ -12,4 +15,21 @@ class Main
     @persons.each { |person| puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   end
 
+  def create_person(person)
+    puts 'Age:'
+    age = gets.chomp
+    puts 'Name:'
+    name = gets.chomp
+    case person
+    when 1
+      puts 'Has parent permission? [Y/N]:'
+      parent_permission = gets.chomp
+      @persons.push(Student.new(age, name, parent_permission))
+    when 2
+      puts 'Specialization:'
+      specialization = gets.chomp
+      @persons.push(Teacher.new(age, name, specialization))
+    end
+    puts 'Person created successfully'
+  end
 end
