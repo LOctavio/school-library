517bab088fdd34d6df839e5861e5d85f5e1f70c3
LOctavio <luis_ram1997@hotmail.com>
2021-10-14 16:09:41 -0500

    Add main method for user interface

--- main.rb
+++ main.rb
@@ -70,4 +70,61 @@ class Main
     end
   end
 
+  def app_info
+    puts 'Welcome to School Library App!'
+    puts ''
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def show_options(option)
+    case option
+    when 1
+      show_books
+    when 2
+      show_people
+    when 6
+      show_rentals
+    end
+  end
+
+  def create_options(option)
+    case option
+    when 3
+      puts 'Do you want to create a student (1) or a teacher (2) [Input the number]: '
+      option = gets.chomp.to_i
+      create_person(option)
+    when 4
+      create_book
+    when 5
+      create_rental
+    end
+  end
+
+  def app_options(option)
+    show_options(option)
+    create_options(option)
+  end
+
+  def main
+    loop do
+      app_info
+      option = gets.chomp.to_i
+      app_options(option)
+      if option == 7
+        puts 'Thank you for using this app!'
+        break
+      end
+      puts ''
+    end
+  end
 end
+
+app = Main.new
+app.main
