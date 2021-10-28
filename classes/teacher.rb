3beed993eed29fa24a97bf85e683c1ff4a30822f
LOctavio <luis_ram1997@hotmail.com>
2021-10-28 12:07:18 -0500

    Fix typo

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,9 +1,9 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, name, specializaton)
+  def initialize(age, name, specialization)
     super(age, name)
-    @specializaton = specializaton
+    @specialization = specialization
   end
 
   def can_use_services?
