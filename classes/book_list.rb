0461c694763c64a99cdc6ab8e89613dbb36858c7
LOctavio <luis_ram1997@hotmail.com>
2021-10-28 11:31:39 -0500

    Store and read info from books.json

--- classes/book_list.rb
+++ classes/book_list.rb
@@ -1,20 +1,31 @@
+require 'json'
+
 class BookList
   attr_accessor :books
 
   def initialize
-    @books = []
+    file = File.open('data/books.json', 'a+')
+    @books = file.size == 0 ? [] : JSON.parse(file.read)
+    file.close
   end
 
   def show
-    @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    file = File.open('data/books.json', 'r')
+    @books.each { |book| puts "Title: \"#{book['title']}\", Author: #{book['author']}" }
+    file.close
   end
 
   def add
+    file = File.open('data/books.json', 'w')
     puts 'Title:'
     title = gets.chomp
     puts 'Author:'
     author = gets.chomp
-    @books.push(Book.new(title, author))
+    book = Book.new(title, author)
+    book = book.to_json
+    @books.push(book)
+    file.write(JSON[@books])
+    file.close
     puts 'Book created successfully'
   end
 end
