3f395da8a04dc7b9b51c6e18cce65295bed26aa6
LOctavio <luis_ram1997@hotmail.com>
2021-10-28 11:57:14 -0500

    Overwrite method to_json for book student

--- classes/student.rb
+++ classes/student.rb
@@ -16,4 +16,14 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
+  def to_json(_options = {})
+    {
+      'className' => self.class.name,
+      'age' => @age,
+      'name' => @name,
+      'parent_permission' => @parent_permission,
+      'classroom' => @classroom
+    }
+  end
 end
