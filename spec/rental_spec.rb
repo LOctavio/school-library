8d71733a43e9dfdf81c54c7c7aa12758afb5faee
LOctavio <luis_ram1997@hotmail.com>
2021-10-29 19:05:29 -0500

    Add unit tests for book class

---
+++ spec/rental_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../classes/rental'
+require_relative '../classes/book'
+require_relative '../classes/student'
+require_relative '../classes/teacher'
+
+describe Rental do
+  context 'Testing the rental class' do
+    before(:context) do
+      date = '12/12/12'
+      @rental = Rental.new(date)
+    end 
+
+    it 'is an instance of Rental' do
+      expect(@rental).to be_an_instance_of(Rental)
+    end
+
+    it 'adds a person to the rental' do
+      @rental.person = Student.new(12, 'luis', true)
+      expect(@rental.person).to be_an_instance_of(Student)
+    end
+    it 'adds a book to the rental' do
+      @rental.book = Book.new('Harry Potter', 'JK Rowling')
+      expect(@rental.book).to be_an_instance_of(Book)
+    end
+  end
+end
